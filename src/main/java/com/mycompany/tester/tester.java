7b0396c8419cace5c9187a4365f3127ffe9865dc
NFear7 <yiyuhuang2007@gmail.com>
2024-05-31 09:44:59 -0400

    first commit
    a bit stuff...

---
+++ src/main/java/com/mycompany/tester/tester.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.tester;
+
+/**
+ *
+ * @author yihua4315
+ */
+public class tester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
